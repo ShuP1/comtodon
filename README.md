b38b400e04e1654fd539c7dc888de9df479529a9
Clement Bois <Clement.Bois@salt.ch>
2019-06-07 15:05:12 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,99 @@
+# Comtodon
+
+A minimal commenting system for static blogs using external [Mastodon](https://joinmastodon.org) or API [compatible](https://pleroma.social) server.
+
+- **painless:** use already existing fediverse accounts
+- **serverless:** hosted on static server or cdn
+- **lightweight:** any useless features *(IMO)*
+- **no dependencies:** pure native js
+- **personal:** no style, no tracking
+
+```html
+<div class="comtodon" data-domain="mastodon.social" data-status="100745593232538751"></div>
+<script src="comtodon.min.js" defer></script>
+```
+
+See [index.html](index.html)
+
+## Deep
+
+Create a tree by adding `data-deep=N`
+  - 0: Full tree
+  - 1: Only "direct" replies
+  - 2: With replies to replies
+  - And so on
+
+## Moderation ?
+
+Add `data-moderator="{moderator_id}"` to display only approved *(replied)* comments
+
+Note: Can't use fav, it requires authentication
+
+## Hugo
+Put `comtodon.min.js` in `static` folder
+
+In your site config
+```yaml
+params:
+  comtodon:
+    domain: mastodon.social
+    # moderator: 358957
+```
+
+In your single page layout
+```html
+{{ if .Params.comtodon }}
+<div class="comtodon" data-domain="{{ .Site.Params.comtodon.domain }}" data-status="{{ .Params.comtodon }}" {{ with .Site.Params.comtodon.moderator }}data-moderator="{{ . }}"{{ end }}></div>
+<script src="//comtodon.min.js" defer></script>
+{{- end }}
+```
+
+In your content header
+```yaml
+comtodon: 100745593232538751
+```
+
+**Style it**
+
+## Mastodon
+
+Only use `/api/v1/statuses/:id/context`. See [doc](https://docs.joinmastodon.org/api/rest/statuses/#get-api-v1-statuses-id-context)
+
+> No authentication required
+
+```
+{
+  descendants: [{
+    account: {
+      acct,
+      avatar_static,
+      display_name,
+      id,
+      emojis: [{shortcode, static_url}],
+      url
+    },
+    created_at,
+    content,
+    emojis: [{shortcode, static_url}],
+    id,
+    in_reply_to_id,
+    sensitive,
+    spoiler_text
+  }]
+}
+```
+
+## No js ?
+
+Simple placeholder
+```html
+<div class="comtodon" data-domain="mastodon.social" data-status="100745593232538751">
+    <p class="no-js sad">Enable JavaScript to see comments</p>
+</div>
+```
+
+## TODO
+
+- Use personal status as sample
+- Create proxy backend for cache and moderating with fav
+- Auto-magically create statuses from hugo ???
---
+++ comtodon.js
@@ -0,0 +1,108 @@
+(function () {
+  function fail(el, text) {
+    el.innerHTML = 'Loading fail =('
+    console.error(text, el)
+  }
+
+  function required(el, field) {
+    const val = el.dataset[field]
+    if (val) {
+      return val
+    }
+    fail(el, `Missing data-${field} attribut`)
+  }
+
+  function h(classes, content, tag = 'div') {
+    return `<${tag} class="${classes}">${content}</${tag}>`
+  }
+
+  const TIMES = new Map([
+    ['second', 1000],
+    ['minute', 60],
+    ['hour', 60],
+    ['day', 24],
+    ['month', 30.5],
+    ['year', 12],
+    ['century', 100]
+  ])
+
+  function ago(date) {
+    const now = Date.now()
+    const target = Number(new Date(date))
+
+    const prefix = target > now ? 'in ' : ''
+    const milliseconds = Math.floor(Math.abs(target - now))
+
+    let cur = 0
+    let divider = 1
+    let name = 'millisecond'
+    for (const time of TIMES) {
+      divider *= time[1]
+      const next = Math.floor(milliseconds / divider)
+      if (next <= 0) {
+        return `${prefix}${cur} ${name}${cur > 1 ? 's' : ''} ago`
+      }
+      name = time[0]
+      cur = next
+    }
+    return `${prefix}a long time ago`
+  }
+
+  function moji(text, emojis) {
+    for (const emoji of emojis) {
+      text = text.split(`:${emoji.shortcode}:`).join(
+        `<img class="emoji" alt="${emoji.shortcode}" title="${emoji.shortcode}" src="${emoji.static_url}">`
+      )
+    }
+    return text
+  }
+
+  function tree(statuses, parent, limit) {
+    const [replies, others] = statuses.reduce(([c, o], s) => (s.in_reply_to_id == parent.id ? [[...c, s], o] : [c, [...o, s]]), [[], []])
+    parent.replies = limit ? replies.map(r => tree(others, r, limit - 1)) : []
+    return parent
+  }
+
+  function html(statuses, domain) {
+    return statuses.map(({ account, created_at, content, id, emojis, sensitive, spoiler_text, replies }) =>
+      h('status', h('date', ago(created_at), 'p') +
+        h('author" href="' + account.url, `<img class="avatar" src="${account.avatar_static}" />` +
+          h('name', moji(account.display_name, account.emojis), 'span') +
+          h('acct', account.acct, 'span'), 'a') +
+        (spoiler_text || sensitive ? h('spoiler', spoiler_text || h('spoiler-empty', 'Sensitive', 'span')) : '') +
+        h('content' + (spoiler_text || sensitive ? ' sensitive' : ''), moji(content, emojis)) +
+        (replies ? h('replies', html(replies, domain)) : '') +
+        h(`reply" href="https://${domain}/interact/${id}?type=reply`, 'Reply', 'a'))).join('')
+  }
+
+  function moderate(statuses, id) {
+    if (!id) {
+      return statuses
+    }
+    const valids = statuses.filter(s => s.account.id == id).map(s => s.in_reply_to_id)
+    return statuses.filter(s => valids.includes(s.id))
+  }
+
+  for (const el of document.getElementsByClassName('comtodon')) {
+    el.innerHTML = '<div class="loading">Loading...</div>'
+
+    const domain = required(el, 'domain')
+    const status = required(el, 'status')
+    if (!domain || !status) {
+      return
+    }
+
+    fetch(`https://${domain}/api/v1/statuses/${status}/context`)
+    .then(res => res.json())
+    .then(res => {
+      el.innerHTML = h(`reply-main" href="https://${domain}/interact/${status}?type=reply`, 'Comment', 'a')
+      const statuses = moderate(res.descendants, el.dataset.moderator)
+      if (statuses) {
+        el.innerHTML += html('deep' in el.dataset ? tree(statuses, { id: status }, el.dataset.deep || -1).replies : statuses, domain)
+      } else {
+        el.innerHTML += h('empty', 'Any comment')
+      }
+      })
+      .catch(() => fail(el, 'Request fail'))
+  }
+})()
